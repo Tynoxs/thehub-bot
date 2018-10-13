6d6be7c2b2af61bdf937dfdd3caf4ead276f5587
Tynoxs <tynoxs@gmail.com>
2018-10-14 00:19:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require("./config.json");
 
 client.on('ready', () => {
   console.log('I am ready!');
@@ -57,4 +56,4 @@ client.on('message', message => {
   
 });
 
-client.login(config.token);
+client.login(process.env.BOT_TOKEN);
